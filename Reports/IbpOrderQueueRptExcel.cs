05aaefbcb08d2b4fbf6891ebcbfc83ca52d9de4d
armsarayut <sarayut.buttathai@gmail.com>
2021-08-26 11:22:09 +0700

    Update Report Excel & PDF

---
+++ Reports/IbpOrderQueueRptExcel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using ClosedXML.Excel.Drawings;
+using GoWMS.Server.Data;
+using GoWMS.Server.Models.Inb;
+
+namespace GoWMS.Server.Reports
+{
+    public class IbpOrderQueueRptExcel
+    {
+        MemoryStream _memoryStream = new MemoryStream();
+        List<Inb_Goodreceive_Go> _Inb_Goodreceive_Go_s = new List<Inb_Goodreceive_Go>();
+        public byte[] Report(List<Inb_Goodreceive_Go> Inb_Goodreceive_Go_s)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.AddWorksheet("Good Receive");
+                #region Excel Report Header
+                var imagePath = VarGlobals.Imagelogoreport();
+                worksheet.Column(1).Width = 18;
+                worksheet.Row(1).Height = 60;
+                var image = worksheet.AddPicture(imagePath).MoveTo(worksheet.Cell("A1")); //this will throw an error
+                image.ScaleWidth(.3);
+                image.ScaleHeight(.2);
+                worksheet.Cell("B1").Value = "Good Receive" + " Report";
+                worksheet.Cell("B1").Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center);
+                worksheet.Cell("B2").Value = $"PrintDate : {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}";
+                #endregion Excel
+
+                #region Excel Report Data
+                var rptRows = 4;
+                worksheet.Cell(rptRows, 1).Value = "QueuDate";
+                worksheet.Cell(rptRows, 2).Value = "Masterpallet";
+                worksheet.Cell(rptRows, 3).Value = "Ducument";
+                worksheet.Cell(rptRows, 4).Value = "PackID";
+                worksheet.Cell(rptRows, 5).Value = "Material";
+                worksheet.Cell(rptRows, 6).Value = "Description";
+                worksheet.Cell(rptRows, 7).Value = "Qty";
+                worksheet.Cell(rptRows, 8).Value = "Unit";
+
+                foreach (var rpt in Inb_Goodreceive_Go_s)
+                {
+                    rptRows++;
+                    worksheet.Cell(rptRows, 1).Value = rpt.Created;
+                    worksheet.Cell(rptRows, 2).Value = rpt.Pallteno;
+                    worksheet.Cell(rptRows, 3).Value = rpt.Docno;
+                    worksheet.Cell(rptRows, 4).Value = rpt.Itemtag;
+                    worksheet.Cell(rptRows, 5).Value = rpt.Itemcode;
+                    worksheet.Cell(rptRows, 6).Value = rpt.Itemname;
+                    worksheet.Cell(rptRows, 7).Value = rpt.Quantity;
+                    worksheet.Cell(rptRows, 8).Value = rpt.Unit;
+                }
+                #endregion
+                workbook.SaveAs(_memoryStream);
+            }
+            return _memoryStream.ToArray();
+        }
+    }
+}
--- Reports/IbpOrderQueueRptPdf.cs
+++ Reports/IbpOrderQueueRptPdf.cs
@@ -8,27 +8,36 @@ using System.Threading.Tasks;
 using GoWMS.Server.Models.Inb;
 using GoWMS.Server.Data;
 
-namespace GoWMS.Server.Pdf
+namespace GoWMS.Server.Reports
 {
-    public class PdfInboundReport
+    public class IbpOrderQueueRptPdf
     {
         readonly BaseFont mpdfFont = BaseFont.CreateFont(VarGlobals.Fontreport(), BaseFont.IDENTITY_H, BaseFont.EMBEDDED); // Setup Font
         #region Declararion
-        int _maxcolum = 8; // Setup Colum table report
+        int _maxcolun = 8;
         Document _document;
         PdfPTable _pdfTable = new PdfPTable(8);
         PdfPCell _pdfCell;
         Font _fontstye;
-       
+        readonly Boolean bPageLanscape = false;
+
         MemoryStream _memoryStream = new MemoryStream();
         List<Inb_Goodreceive_Go> _Inb_Goodreceive_Go_s = new List<Inb_Goodreceive_Go>();
-  
+
         #endregion
         public byte[] Report(List<Inb_Goodreceive_Go> Inb_Goodreceive_Go_s)
         {
             _Inb_Goodreceive_Go_s = Inb_Goodreceive_Go_s;
-
-            _document = new Document(PageSize.A4, 10f, 10f, 30f, 20f);// Setup page
+            if (bPageLanscape)
+            {
+                _document = new Document(PageSize.A4.Rotate(), 10f, 10f, 30f, 10f); // Setup page Lascape
+            }
+            else
+            {
+                _document = new Document(PageSize.A4, 10f, 10f, 30f, 10f); // Setup page Protrait
+            }
+            
+            
             _pdfTable.WidthPercentage = 100;
             _pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
 
@@ -38,10 +47,17 @@ namespace GoWMS.Server.Pdf
             _document.Open();
             this.ReportLogo();
 
-            float[] sizes = new float[_maxcolum];
-            for (var i = 0; i < _maxcolum; i++) // Setup Colum Size
+            float[] sizes = new float[_maxcolun];
+            for (var i = 0; i < _maxcolun; i++) // Set up Colum Size
             {
-                if (i == 0) sizes[i] = 50;
+                if (i == 0) sizes[i] = 70;
+                else if (i == 1) sizes[i] = 70;
+                else if (i == 2) sizes[i] = 90;
+                else if (i == 3) sizes[i] = 70;
+                else if (i == 4) sizes[i] = 100;
+                else if (i == 5) sizes[i] = 150;
+                else if (i == 6) sizes[i] = 70;
+                else if (i == 7) sizes[i] = 50;
                 else sizes[i] = 100;
             }
             _pdfTable.SetTotalWidth(sizes);
@@ -78,73 +94,85 @@ namespace GoWMS.Server.Pdf
         {
             iTextSharp.text.Image png = iTextSharp.text.Image.GetInstance(GoWMS.Server.Data.VarGlobals.Imagelogoreport());
             png.ScaleAbsolute(40, 40);
-            png.SetAbsolutePosition(10, 800);
+            if (bPageLanscape)
+            {
+                png.SetAbsolutePosition(10, 550);
+            }
+            else
+            {
+                png.SetAbsolutePosition(10, 797);
+            }
+                
             _document.Add(png);
         }
 
         private void ReportBody()
         {
-            _fontstye = new Font(mpdfFont, 9f, 0); // FontFactory.GetFont("Tahoma", 9f, 1);
+            _fontstye = new Font(mpdfFont, 9f, 0);
             #region Table Header
-            _pdfCell = new PdfPCell(new Phrase("No.", _fontstye));
-            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
-            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
-            _pdfCell.BackgroundColor = BaseColor.LightGray;
+            iTextSharp.text.BaseColor headerBackcolor = BaseColor.LightGray;
+
+            _pdfCell = new PdfPCell(new Phrase("QueuDate", _fontstye))
+            {
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
+            };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Masterpallet", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Ducument", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("PackID", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Material", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Description", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Qty", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
             _pdfCell = new PdfPCell(new Phrase("Unit", _fontstye))
             {
                 HorizontalAlignment = Element.ALIGN_CENTER,
-                VerticalAlignment = Element.ALIGN_MIDDLE,
-                BackgroundColor = BaseColor.LightGray
+                VerticalAlignment = Element.ALIGN_CENTER,
+                BackgroundColor = headerBackcolor
             };
             _pdfTable.AddCell(_pdfCell);
 
@@ -152,71 +180,71 @@ namespace GoWMS.Server.Pdf
             #endregion
 
             #region Table Body
-            _fontstye = new Font(mpdfFont, 9f, 0); // FontFactory.GetFont("Tahoma", 9f, 1);
-            int nSL = 1;
-            foreach (var Inb_Goodreceive_Go_s in _Inb_Goodreceive_Go_s)
+            _fontstye = new Font(mpdfFont, 9f, 0);
+            iTextSharp.text.BaseColor bodyBackcolor = BaseColor.White;
+            foreach (var listRpt in _Inb_Goodreceive_Go_s)
             {
-                _pdfCell = new PdfPCell(new Phrase(nSL++.ToString(), _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Created.ToString(), _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Pallteno, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Pallteno, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Docno, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Docno, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Itemtag, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Itemtag, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Itemcode, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Itemcode, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Itemname, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Itemname, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Quantity.ToString(), _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Quantity.ToString(), _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
-                _pdfCell = new PdfPCell(new Phrase(Inb_Goodreceive_Go_s.Unit, _fontstye))
+                _pdfCell = new PdfPCell(new Phrase(listRpt.Unit, _fontstye))
                 {
                     HorizontalAlignment = Element.ALIGN_CENTER,
                     VerticalAlignment = Element.ALIGN_MIDDLE,
-                    BackgroundColor = BaseColor.White
+                    BackgroundColor = bodyBackcolor
                 };
                 _pdfTable.AddCell(_pdfCell);
 
